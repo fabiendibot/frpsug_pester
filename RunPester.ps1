d4c19717ce1d9f612162a266d42a97da6a086f89
Fabien Dibot <fabien.dibot@gmail.com>
2016-12-02 11:25:20 +0100

    fix few things

--- RunPester.ps1
+++ RunPester.ps1
@@ -5,105 +5,61 @@ Ce script lance des tests Pester avec Jenkins
  
 .DESCRIPTION
 Ce script par le biais d'une connexion WinRM va lancer l'import du module Pester pour faire des tests unitaires sur la configuration du IIS.
-Une fois le module importé, il lance les tests qui sont dans sont répertoire courant, puis génère un rapport au format XML NUnit que Jenkins 
-est capable d'interpréter pour donner des informations fiables sur l'état du déploiement.
-Ce rapport sera situé dans le chemin correspondant à la variable $Source.
+Une fois le module importï¿½, il lance les tests qui sont dans sont rï¿½pertoire courant, puis gï¿½nï¿½re un rapport au format XML NUnit que Jenkins 
+est capable d'interprï¿½ter pour donner des informations fiables sur l'ï¿½tat du dï¿½ploiement.
+Ce rapport sera situï¿½ dans le chemin correspondant ï¿½ la variable $Source.
 
 Pour lancer ce script dans Jenkins:
 
  
 .PARAMETER Source
-Correspond à la variable %WORKPLACE% dans Jenkins.
+Correspond ï¿½ la variable %WORKPLACE% dans Jenkins.
  
 .PARAMETER Target
 C'est le serveur IIS sur lequel vous voulez jouer les tests.
 
 .PARAMETER IISRoot
-Correspond à la racine du site IIS à tester.
+Correspond ï¿½ la racine du site IIS ï¿½ tester.
 
 .PARAMETER LifePage
-URL de la page de vie à tester
+URL de la page de vie ï¿½ tester
 
 .PARAMETER IISSite
-Nom du site web à tester avec Pester
+Nom du site web ï¿½ tester avec Pester
 
 .PARAMETER IISPool
 Nom du pool d'application qui contient le site web pour le tester avec Pester
 
 .PARAMETER TestName
-Correspond au nom du jeu de tests à jouer.
+Correspond au nom du jeu de tests ï¿½ jouer.
 
 .PARAMETER ApplicationVersion
-Version de l'application qui doit être testé avec Pester
+Version de l'application qui doit ï¿½tre testï¿½ avec Pester
 
 .PARAMETER ApplicationName
-Nom de l'application qui doit être testé avec Pester
+Nom de l'application qui doit ï¿½tre testï¿½ avec Pester
 
 .PARAMETER OutFile
-Correspond au nom du fichier du rapport XML qui va être généré.
+Correspond au nom du fichier du rapport XML qui va ï¿½tre gï¿½nï¿½rï¿½.
 
 .PARAMETER DriveLetter
-Correspond à la lettre du lecteur réseau temporaire qui va être créé pour copier le fichier XML de résultat.
+Correspond ï¿½ la lettre du lecteur rï¿½seau temporaire qui va ï¿½tre crï¿½ï¿½ pour copier le fichier XML de rï¿½sultat.
  
 
 #>
 
  param (
     [Parameter(Mandatory=$true)]
- 	[String]$Source,
-    [String]$Target,
-    [String]$IISRoot,
-    [String]$TestName,
     [String]$OutFile,
-    [String]$DriveLetter,
-    [String]$LifePage,
     [String]$IISSite,
-    [String]$IISPool,
-    [String]$ApplicationVersion,
-    [String]$ApplicationName
+    [String]$IISPool
 
  )
 
  Try {
-     # Vérification si le lecteur $Driveletter n'est pas déjà utilisé et qu'on ne copie pas le framework Pester et les TU n'importe ou.
-     Write-Verbose "Check si le lecteur $DriveLetter est déjà utilisé."
-     if (Test-Path $DriveLetter) {
-        Write-Error "Le lecteur $DriveLetter ne doit pas être déjà présent. \n Afin d'éviter des erreurs de copie, l'exécution de ce script va s'arrêter."
-        exit 1
-     }
-
-
-
-      # Exécution des tests Pester
-     # On importe le module dans la session courante
-     Write-verbose "Import du module Pester et exécution des tests d'infra sur $Target."
-
-    
-        # On se place dans le répertoire Pester pour pouvoir lancer les tests
- 	    cd "$source\TestsPester"
-
-        # On importe le framework Pester
- 	    Import-Module "$($objForRemote.IISRoot)\Pester\Pester.psm1" 
-        Import-Module "$($objForRemote.IISRoot)\Remotely\Remotely.psm1"
-
-        # On exécute les tests
- 	    Invoke-Pester -verbose -OutputFile $OutFile -OutputFormat NUnitXml `                      -EnableExit -Script @{ Path = "$Source\TestsPester"; Parameters = @{LifePage = $LifePage;
-                                                             IISSite = $IISSite;
-                                                             IISPool = $IISPool;
-                                                             ApplicationVersion = $ApplicationVersion;
-                                                             ApplicationName = $ApplicationName};}
- 
-     # Connexion au partage réseau pour récupérer le fichier XML
-     Write-Verbose "Connexion au lecteur réseau $DriveLetter mappé sur \\$Target\d$"
-     $net = new-object -ComObject WScript.Network
-     $net.MapNetworkDrive($DriveLetter, "\\$Target\d$")
- 
-     # COpie du fichier de résultat des tests Pester
-     Copy-Item -Path "$DriveLetterInetpub\collecte-rmoe-ccv-poc\TestsPester\$OutFile" -Destination "$Source\$OutFile"
- 
-     # Déconnexion du lecteur réseau
-     Write-Verbose "Déconnexion du lecteur réseau $DriveLetter"
-     $net.RemoveNetworkDrive($DriveLetter)
+ 	    Invoke-Pester -verbose -OutputFile $OutFile -OutputFormat NUnitXml `
+                      -EnableExit -Script @{ Path = "Tests"; Parameters = @{IISSite = $IISSite;
+                                                             IISPool = $IISPool};}
  }
  Catch {
     Write-Output "Test"
--- Tests/BDD.tests.ps1
+++ Tests/BDD.tests.ps1
@@ -1,47 +1,22 @@
 ﻿param (
-    [String]$LifePage,
     [String]$IISSite,
-    [String]$IISPool,
-    [String]$ApplicationVersion,
-    [String]$ApplicationName
+    [String]$IISPool
 )
 
-Describe "ValidateCCV" {
+Describe "ValidateIIS" {
     
-    #Import-Module webadministration
-
-    # Récupération des variables dans le fichier .xml
-        
-    $Result  = Invoke-WebRequest -Uri $LifePage
-
     # Check if IIS is installed
     It "IIS Service Started" {
-        Remotely { (Get-Service W3SVC).Status } | Should be "Running"
+         (Get-Service W3SVC).Status Should be "Running"
     }
 
     # Check if Website is created
     It "IIS Web Site started" {
-        Remotely { param($IISSite) (Get-Website -Name $IISSite).State } -ArgumentList $IISSite | Should be "Started"
+          (Get-Website -Name $IISSite).State | Should be "Started"
     }
     
     # Check if Application exsists
     It "IIS Application Pool Started" {
-        Remotely { (Get-WebAppPoolState -name $IISPool).Value } | Should be "Started"
+         (Get-WebAppPoolState -name $IISPool).Value Should be "Started"
     }
-
-    # Check application version
-    It "Application version" {
-        Remotely { param($Result) ($Result.AllElements | ? { $_.id -eq 'applicationVersion' }).outerText } -ArgumentList $Result | Should be $($ApplicationVersion)
-    }
-
-    # Check Application Name
-    It "Application Name" {
-        Remotely { param($Result) ($Result.AllElements | ? { $_.id -eq 'NomApplication' }).outerText } -ArgumentList $Result | Should be $($ApplicationName)
-    }
-
-    # Check Database Connexion
-    It "Database Connexion" {
-        Remotely { param($Result) ($Result.AllElements | ? { $_.id -eq 'testAppelBdd' }).outerText } -ArgumentList $Result | Should be 'KO'
-    }
-
 }
