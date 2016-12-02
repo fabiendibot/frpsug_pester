12dee7b91b0648d5ae5121693432ab8de1b26257
Fabien Dibot <fabien.dibot@gmail.com>
2016-12-02 11:32:51 +0100

    Jobijoba

--- RunPester.ps1
+++ RunPester.ps1
@@ -4,25 +4,6 @@
 Ce script lance des tests Pester avec Jenkins
  
 .DESCRIPTION
-Ce script par le biais d'une connexion WinRM va lancer l'import du module Pester pour faire des tests unitaires sur la configuration du IIS.
-Une fois le module import�, il lance les tests qui sont dans sont r�pertoire courant, puis g�n�re un rapport au format XML NUnit que Jenkins 
-est capable d'interpr�ter pour donner des informations fiables sur l'�tat du d�ploiement.
-Ce rapport sera situ� dans le chemin correspondant � la variable $Source.
-
-Pour lancer ce script dans Jenkins:
-
- 
-.PARAMETER Source
-Correspond � la variable %WORKPLACE% dans Jenkins.
- 
-.PARAMETER Target
-C'est le serveur IIS sur lequel vous voulez jouer les tests.
-
-.PARAMETER IISRoot
-Correspond � la racine du site IIS � tester.
-
-.PARAMETER LifePage
-URL de la page de vie � tester
 
 .PARAMETER IISSite
 Nom du site web � tester avec Pester
@@ -30,21 +11,9 @@ Nom du site web � tester avec Pester
 .PARAMETER IISPool
 Nom du pool d'application qui contient le site web pour le tester avec Pester
 
-.PARAMETER TestName
-Correspond au nom du jeu de tests � jouer.
-
-.PARAMETER ApplicationVersion
-Version de l'application qui doit �tre test� avec Pester
-
-.PARAMETER ApplicationName
-Nom de l'application qui doit �tre test� avec Pester
-
 .PARAMETER OutFile
 Correspond au nom du fichier du rapport XML qui va �tre g�n�r�.
 
-.PARAMETER DriveLetter
-Correspond � la lettre du lecteur r�seau temporaire qui va �tre cr�� pour copier le fichier XML de r�sultat.
- 
 
 #>
 
